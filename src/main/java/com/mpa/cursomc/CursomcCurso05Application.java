2095f32bfff1d05b032e62395aef3647f1e6b117
Marcelo Almeida <mpaemail@gmail.com>
2020-01-30 20:53:56 -0300

    Projeto criado.

---
+++ src/main/java/com/mpa/cursomc/CursomcCurso05Application.java
@@ -0,0 +1,13 @@
+package com.mpa.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcCurso05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcCurso05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpa/cursomc/CursomcCurso05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpa.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcCurso05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
